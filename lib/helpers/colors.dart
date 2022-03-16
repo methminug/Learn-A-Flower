700093c349943d19b09bd4d87a8ed36bb0cd2ba8
Methmi Nugawela <methmi.nuga@gmail.com>
2022-03-16 18:41:15 +0530

    Created basic app structure

---
+++ lib/helpers/colors.dart
@@ -0,0 +1,10 @@
+//Feel free to change!
+
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFF97DBAE);
+  static const success = Color(0xFFC3E5AE);
+  static const fail = Color.fromARGB(255, 255, 103, 115);
+  static const warning = Color(0xFFF1E1A6);
+}
---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,5 @@
+//All routes of application
+
+class AppRoutes {
+  static const HOME = '/home';
+}
